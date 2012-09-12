2fe909134da9d9acfb5bbe54c07dc353714d1c9b
teacher73 <net94@nate.com>
2012-09-12 14:19:03 +0900

    연습문제 추가 5번, 6번

--- src/chapter02/Method02.java
+++ src/chapter02/Method02.java
@@ -1,41 +1,26 @@
+/**
+ *
+ * @package		: chapter02
+ * @FileName	: Method04.java
+ * @Date  		: 2012. 9. 11.
+ * @version  	: 1.0.0
+ * @Comment  	: 명령행 파라미터를 출력하는 프로그램
+ *
+ */
+
 package chapter02;
 
-import java.util.Scanner;
+/**
+ * 명령행 파라미터를 출력하는 프로그램
+ * @author	MinSu Kim (net94@nate.com)
+ * @since	2012. 9. 11. 오후 11:34:19
+ * @version	1.0.0
+ */
 
 public class Method02 {
 	public static void main(String[] args) {
-		String[] sbj = {"국어", "영어", "수학", "총점", "평균"};
-		float[][] table = new float[3][5];
-		Scanner sc = new Scanner(System.in);
-		
-		for(int i=0; i<table.length; i++){
-			System.out.println("성적을 입력하세요..");
-			float total=0;
-			
-			for(int j=0; j<table[i].length-2; j++){
-				System.out.print(sbj[j] + "점수 입력 ");
-				table[i][j] = sc.nextFloat();
-				total +=table[i][j];
-			}
-
-			table[i][table[i].length-2] = total;
-			table[i][table[i].length-1] = total/3;
-			/*System.out.println(table[i][0] + "\t"+ table[i][1] + "\t"+table[i][2] + "\t"+table[i][3] + "\t"+table[i][4] + "\t");*/
-		}
-		
-		printArr(table, sbj);
-	}
-
-	private static void printArr(float[][] table, String[] subj) {
-		System.out.println("============================");;
-		for(int i=0; i<subj.length; i++)System.out.print(subj[i]+ "\t");
-		System.out.println();
-		
-		for(int i=0; i<table.length; i++){
-			for(int j=0; j<table[i].length; j++){
-				System.out.printf("%3.1f \t",table[i][j]);
-			}
-			System.out.println();
-		}
+		for(String str: args)
+			System.out.println(str);
+		System.out.println("args.length=" + args.length);
 	}
 }
--- src/chapter02_test/Ex02_6.java
+++ src/chapter02_test/Ex02_6.java
@@ -1,8 +1,8 @@
-package chapter02;
+package chapter02_test;
 
 import java.util.Scanner;
 
-public class Method03 {
+public class Ex02_6 {
 	public static void main(String[] args) {
 		String[] sbj = {"국어", "영어", "수학", "총점", "평균"};
 		float[][] table = new float[3][5];
@@ -36,6 +36,7 @@ public class Method03 {
 		System.out.println("============================");;
 		for(int i=0; i<subj.length; i++)System.out.print(subj[i]+ "\t");
 		System.out.println();
+		float totKor=0.0F, totEng=0.0F, totMath=0.0F, totSum=0.0F, totAvg=0.0F;
 		
 		for(int i=0; i<table.length; i++){
 			for(int j=0; j<table[i].length; j++){
@@ -43,5 +44,16 @@ public class Method03 {
 			}
 			System.out.println();
 		}
+		for(int i=0; i<table.length; i++){
+			totKor +=table[i][0];
+			totEng +=table[i][1];
+			totMath +=table[i][2];
+			totSum +=table[i][3];
+			totAvg +=table[i][4];
+		}
+		System.out.println("============================");
+		System.out.printf("%3.1f \t%3.1f\t%3.1f\t%3.1f\t%3.1f",
+				totKor/table.length,totEng/table.length, totMath/table.length, totSum/table.length, totAvg/table.length);
+		
 	}
 }
