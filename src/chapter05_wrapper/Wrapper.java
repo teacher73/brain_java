8cbe58c336df9bd7551035fad2692f0c8bda06fc
teacher73 <net94@nate.com>
2012-09-23 13:13:36 +0900

    main 수정

--- src/chapter05_wrapper/Wrapper.java
+++ src/chapter05_wrapper/Wrapper.java
@@ -19,26 +19,7 @@ package chapter05_wrapper;
 
 public class Wrapper {
 	public static void main(String[] args){
-		Integer obj =new Integer(4);
-		Integer obj1 = new Integer(5);
 		
-		System.out.println(obj.getClass().getSimpleName() + obj);
-		System.out.println(obj1.getClass().getSimpleName() + obj1);
-		swap(obj, obj1);
-		System.out.println(obj.getClass().getSimpleName() + obj);
-		System.out.println(obj1.getClass().getSimpleName() + obj1);
 	}
 
-	/**
-	 * 
-	 * @param obj
-	 * @param obj1
-	 */
-	
-	
-	private static void swap(Integer obj, Integer obj1) {
-		Integer temp=obj;
-		obj = obj1;
-		obj1 = temp;
-	}
 }
