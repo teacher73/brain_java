e9745ee6e4db8ac204c6382cd63860fbaf493dac
teacher73 <net94@nate.com>
2013-01-13 13:30:16 +0900

    da0 add

---
+++ src/studentmanager/DeleteStd.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @package		: studentmanager
+ * @FileName	: DeleteStd.java
+ * @Date  		: 2013. 1. 13.
+ * @version  	: 1.0.0
+ * @Comment  	:
+ *
+ */
+
+package studentmanager;
+
+/**
+ * 
+ * @author	mskim
+ * @since	2013. 1. 13. 오후 1:29:22
+ * @version	1.0.0
+ */
+
+public interface DeleteStd {
+
+}
---
+++ src/studentmanager/UpdateStd.java
@@ -0,0 +1,22 @@
+/**
+ *
+ * @package		: studentmanager
+ * @FileName	: UpdateStd.java
+ * @Date  		: 2013. 1. 13.
+ * @version  	: 1.0.0
+ * @Comment  	:
+ *
+ */
+
+package studentmanager;
+
+/**
+ * 
+ * @author	mskim
+ * @since	2013. 1. 13. 오후 1:29:17
+ * @version	1.0.0
+ */
+
+public interface UpdateStd {
+
+}
